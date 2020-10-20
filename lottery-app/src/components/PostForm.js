ab2b00d724cfff4dd7f69ba9c67da5fd03dbd61b
Nathan Sanford <nwsanfor24@gmail.com>
2020-10-20 08:57:35 -0400

    able to fetch from json and submit form to API

--- lottery-app/src/components/PostForm.js
+++ lottery-app/src/components/PostForm.js
@@ -7,21 +7,47 @@ class PostForm extends Component {
             title: '',
             body: ''
         };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
     }
 
     render() {
         return (
             <div>
                 <h1>Add Post</h1>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>Title: </label><br />
-                        <input type="text" name="title" value={this.state.title} />
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
                     </div>
                     <br />
                     <div>
                         <label>Body: </label><br />
-                        <textarea name="body" value={this.state.body} />
+                        <textarea name="body" onChange={this.onChange} value={this.state.body} />
                     </div>
                     <br />
                     <button type="submit">Submit</button>
