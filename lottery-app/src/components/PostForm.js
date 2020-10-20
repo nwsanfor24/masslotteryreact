9856fce1e42c8efdf7d4714a32de6bc2ac19d67d
Nathan Sanford <nwsanfor24@gmail.com>
2020-10-19 20:54:22 -0400

    Made a boilerplate form on react

--- lottery-app/src/components/PostForm.js
+++ lottery-app/src/components/PostForm.js
@@ -3,13 +3,29 @@ import React, { Component } from 'react'
 class PostForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            title: '',
+            body: ''
+        };
     }
 
     render() {
         return (
             <div>
                 <h1>Add Post</h1>
+                <form>
+                    <div>
+                        <label>Title: </label><br />
+                        <input type="text" name="title" value={this.state.title} />
+                    </div>
+                    <br />
+                    <div>
+                        <label>Body: </label><br />
+                        <textarea name="body" value={this.state.body} />
+                    </div>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
