ece37ef71bb3988641a43d4cdbf263d2ea52e89b
Nathan Sanford <nwsanfor24@gmail.com>
2020-10-20 19:39:19 -0400

    setting up more boilerplate for redux

--- lottery-app/src/App.js
+++ lottery-app/src/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
-  );
+import store from './store';
+
+class App extends Component {
+  render() {
+    return(
+      <Provider store={store}>
+        <div className="App">
+        < header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
