6064e50c7dcc9baf01941985bfe6e89fc94f94f8
Nathan Sanford <nwsanfor24@gmail.com>
2020-10-20 20:31:30 -0400

    postReducer is complete

--- lottery-app/src/store.js
+++ lottery-app/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -8,8 +8,12 @@ const middleware = [thunk];
 
 const store = createStore(
     rootReducer, 
-    intitialState, 
-    applyMiddleware(...middleware)
+    intitialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+    ) 
+    
 );
 
 export default store;
