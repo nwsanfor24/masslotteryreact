970ea44e3db86a9bf6fcb6a6d4dd7fe3932b67be
Nathan Sanford <nwsanfor24@gmail.com>
2020-10-21 15:38:30 -0400

    mass lottery rest api won't work, trouble shooting design

--- lottery-app/src/actions/postActions.js
+++ lottery-app/src/actions/postActions.js
@@ -1,27 +1,27 @@
 import { FETCH_POSTS, NEW_POST } from './types';
 
 export const fetchPosts = () => dispatch => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://www.masslottery.com/rest/keno/getDrawsByDateRange?startDate=2020-09-18&endDate=2020-09-18')
         .then(res => res.json())
-        .then(posts => dispatch({
+        .then(res => dispatch({
             type: FETCH_POSTS,
-            payload: posts
+            payload: res
         }));
 };
 
 
 
-export const createPost = (postData) => dispatch => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(postData)
-        })
-        .then(res => res.json())
-        .then(post => dispatch({
-            type: NEW_POST,
-            payload: post
-        }));  
-}
+// export const createPost = (postData) => dispatch => {
+//     fetch('https://www.masslottery.com/rest/keno/getDrawsByDateRange?startDate=2020-09-18&endDate=2020-09-18', {
+//             method: 'POST',
+//             headers: {
+//                 'content-type': 'application/json'
+//             },
+//             body: JSON.stringify(postData)
+//         })
+//         .then(res => res.json())
+//         .then(post => dispatch({
+//             type: NEW_POST,
+//             payload: post
+//         }));  
+// }
